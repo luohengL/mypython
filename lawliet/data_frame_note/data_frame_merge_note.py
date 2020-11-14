7f60cdc5b413ba580d6a3014a9911c716ff3852a
luoheng <ls299792>
2020-11-14 17:48:43 +0800

    dataframe

---
+++ lawliet/data_frame_note/data_frame_merge_note.py
@@ -0,0 +1,36 @@
+# @Time    : 2020/11/4 3:29 下午
+# @Author  : luoh
+# @Email   : luohenghlx@163.com
+# @File    : data_frame_query_note.py
+# @Software: PyCharm
+# @Description: dataFrame 创建
+
+import pandas as pd
+import numpy as np
+
+
+"""
+DataFrame是一种表格型数据结构，它含有一组有序的列，每列可以是不同的值。DataFrame既有行索引，也有列索引，它可以看作是由Series组成的字典，不过这些Series公用一个索引。
+DataFrame的创建有多种方式，不过最重要的还是根据dict进行创建，以及读取csv或者txt文件来创建。这里主要介绍这两种方式。
+
+作者：文哥的学习日记
+链接：https://www.jianshu.com/p/8024ceef4fe2
+来源：简书
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+"""
+
+
+
+frame_a = pd.DataFrame({'fuse_code': ['one', 'two', 'three'],
+                    'data1': np.arange(3)})
+print(frame_a)
+
+frame_b = pd.DataFrame({'fuse_code': ['one', 'three', 'four'],
+                    'data2': np.arange(3)})
+print(frame_b)
+
+frame_total=pd.merge(frame_a,frame_b,on='fuse_code',how='left')
+print(frame_total)
+
+
+
